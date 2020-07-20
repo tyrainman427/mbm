c20ee67e59583c43c984fea8de72b2db9a1986fb
Ty rainey <etnuh@Tys-MacBook-Pro.local>
2020-07-20 14:17:02 -0400

    update view on news page

--- MBM/MBM/settings.py
+++ MBM/MBM/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'members',
     'employee',
     'mublog',
+    'news',
 ]
 
 MIDDLEWARE = [
--- MBM/members/models.py
+++ MBM/members/models.py
@@ -7,14 +7,24 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=500)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zip = models.CharField(max_length=5)
+    
+    def __str__(self):
+        return f"{self.street},{self.city},{self.state}"
+
 class Member(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
+    date_of_birth = models.DateField()
     phone_number = models.CharField(max_length=250, blank=True)
     email_address = models.CharField(max_length=30)
     date_created = models.DateField(auto_now_add=True)
     is_admin = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.first_name
@@ -22,15 +32,6 @@ class Member(models.Model):
     # def get_absolute_url(self): # new
     #     return reverse('members:members_detail', args=[str(self.id)])
 
-class Address(models.Model):
-    street = models.CharField(max_length=500)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=2)
-    zip = models.CharField(max_length=5)
-    member = models.ForeignKey(Member, on_delete=models.CASCADE, null=True)
-    
-    def __str__(self):
-        return self.street
     
 class Membership(models.Model):
     is_active = models.BooleanField(default=True)
